758227f7785e87e2b487294f1479780311fedd7a
Nick <Nicholashathaway@gmail.com>
2017-12-15 10:47:38 -0700

    add more competency comments

--- src/components/AllResults.js
+++ src/components/AllResults.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 export default class AllResults extends Component {
   constructor() {
     super();
+    // COMP 36C
     this.state = { term: '' }
   }
 
   componentDidMount() {
-    // COMP 42J, 42K
+    // COMP 42J, 42K, 36D
     this.setState({ term: this.props.match.params.term })
   }
 
+  // COMP 36F
   render() {
     console.log('ALLRESULTS STATE', this.state);
     console.log('ALLRESULTS PROPS', this.props);
